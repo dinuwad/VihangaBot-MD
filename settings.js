dacc2f16486fba0ce5d7dbc2cc4750b0c55f7236
dinuwad <112315195+dinuwad@users.noreply.github.com>
2022-08-28 14:43:43 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,19 +21,19 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['94763427338']
-global.premium = ['94763427338']
-global.ownernomer = '94763427338'
+global.owner = ['94740804536']
+global.premium = ['94740804536]
+global.ownernomer = '94740804536'
 global.ownername = '៚ᴠɪʜͥᴀɴͣɢͫᴀ ʏᴛ͢⁸⁵⁴¹⃟✦⃝̮̮〽️'
-global.botname = 'VIHANGA MD BOT'
-global.footer = 'VIHANGA MD'
-global.cap = '*Genarated By Vihanga Md*'
+global.botname =  NOWTY DINUWA  MD BOT'
+global.footer = '❤️ NOWTY DINUWA ❤️'
+global.cap = '*Genarated By NOWTY DINUWA  Md*'
 global.ig = 'https://github.com/vihangabot'
-global.region = 'Sri Lanka, Sabaragamuwa, Embilipitiya'
+global.region = 'Sri Lanka , Homagama ,wellipillawa '
 global.sc = 'https://github.com/vihangabot'
-global.myweb = 'https://chat.whatsapp.com/BDge3i6chrQKWkkSRellyT'
-global.packname = 'Vihanga Md Sticker Bot'
-global.author = '៚ᴠɪʜͥᴀɴͣɢͫᴀ ʏᴛ͢⁸⁵⁴¹⃟✦⃝̮̮〽️'
+global.myweb = 'https://chat.whatsapp.com/IKbvhlnSXFWDnW7ZxSWwLN'
+global.packname = ' Nowty Dinuwa  Md Sticker Bot'
+global.author = '៚ Nowty Dinuwa '
 global.autobio = 'true'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
